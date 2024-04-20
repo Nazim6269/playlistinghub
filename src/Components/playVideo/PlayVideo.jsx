87a9bb0166969b320415845fe952a937184de6db
Nazim Uddin <107185368+Nazim6269@users.noreply.github.com>
2024-04-20 11:31:38 +0600

    ✨ feat: integrate react-youtube package for video playback

--- src/Components/playVideo/PlayVideo.jsx
+++ src/Components/playVideo/PlayVideo.jsx
@@ -1,17 +1,23 @@
 import { Container } from "@mui/material";
 import React from "react";
 import { useParams } from "react-router-dom";
+import YouTube from "react-youtube";
 
 const PlayVideo = () => {
   const { videoId } = useParams();
+
+  // Options for the YouTube player
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      autoplay: 0, // Set to 0 to prevent autoplay
+    },
+  };
+
   return (
     <Container sx={{ marginTop: 20 }}>
-      <iframe
-        className="video"
-        title="Youtube player"
-        sandbox="allow-same-origin allow-forms allow-popups allow-scripts allow-presentation"
-        src={`https://youtube.com/embed/${videoId}?autoplay=0`}
-      ></iframe>
+      <YouTube videoId={videoId} opts={opts} />
     </Container>
   );
 };
